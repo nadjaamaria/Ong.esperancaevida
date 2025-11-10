042ce0ddd5d4f903b8001012a631a7a9a7913df8
Nadja Maria Simão <nadjamariasimao@gmail.com>
2025-11-10 14:27:37 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Mensagem de boas-vindas no console
+console.log("Bem-vindo à plataforma da ONG Esperança Viva!");
+
+// Exemplo: mensagem quando o formulário é enviado
+document.addEventListener("submit", function() {
+    alert("Cadastro enviado com sucesso!");
+});
